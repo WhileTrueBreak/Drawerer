d2e7f3afe69e390f3c546c0e2b01a2108c4e673e
whiletrue <jche0470@student.monash.edu>
2024-01-12 15:59:02 +1100

    .

--- src/application/Drawerer.java
+++ src/application/Drawerer.java
@@ -307,7 +307,7 @@ public class Drawerer extends RoboticsAPIApplication{
 		
 		logger.info("Reading Path File");
 		String resPath = FileReader.findUniqueFolder("res", "..");
-		List<String> file = FileReader.readFile(resPath+"/linie_c.txt");
+		List<String> file = FileReader.readFile(resPath+"/frieren_c.txt");
 
 		PathPlan plan = createPathPlanV1(file, originFrame, canvas);
 //		PathPlan plan = createPathPlanV2(file, originFrame, canvas);
